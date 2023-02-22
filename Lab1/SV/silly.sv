757dc3110e97a1e2857f078765255da10ba1a29f
RicardoxCruz <126110251+RicardoxCruz@users.noreply.github.com>
2023-02-22 13:38:11 -0600

    uploaded code

--- Lab1/SV/silly.sv
+++ Lab1/SV/silly.sv
@@ -1,5 +1,15 @@
-module silly (input  logic a, b, c, output logic y);
+module fulladder (input  logic a, b, c, output logic out, cout);
+  assign out = a ^ b ^ c;
+  assign cout = a & b | a & c | b & c;
    
-  assign y = ~b & ~c | a & ~b;
-   
+endmodule
+
+
+module rca (input logic [3:0] a, b, input logic [0:0] c, output logic [3:0] out, cout);
+
+fulladder fa1 (a[0], b[0], c[0], out[0], cout[0]);
+fulladder fa2 (a[1], b[1], cout[0], out[1], cout[1]);
+fulladder fa3 (a[2], b[2], cout[1], out[2], cout[2]);
+fulladder fa4 (a[3], b[3], cout[2], out[3], cout[3]);
+
 endmodule
--- Lab1/SV/silly_tb.sv
+++ Lab1/SV/silly_tb.sv
@@ -1,14 +1,19 @@
 `timescale 1ns / 1ps
 module tb ();
 
-   logic        a;
-   logic 	b;
-   logic 	c;
-   logic 	y;
-   logic        clk;   
+   logic [3:0] a; // 4 bit input
+   logic [3:0] b; // 4 bti input
+   logic [0:0] c;
+   logic [3:0] out;
+   logic [3:0] cout;  
+   logic [3:0] Sum;
+   logic [3:0] Sum_corr;
+   logic [3:0] Sum_correct;
+
+   logic 	clk;
    
   // instantiate device under test
-   silly dut (a, b, c, y);
+   rca dut (a, b, c, out, cout);
  ////////////////////////////////////////////////////////////////////
    // 20 ns clock
    initial 
@@ -17,45 +22,104 @@ module tb ();
 	forever #10 clk = ~clk;
      end
 
+	integer handle3;
+	integer desc3;
+	integer i;
+	
+	
+	initial
+		begin
+			handle3 = $fopen("rca.out");
+			desc3 = handle3;
+			#1250 $finish;
+	end
+
+	initial
+		begin
+		for (i=0; i < 128; i=i+1)
+			begin
+
+				@(posedge clk)
+					begin
+							#20 a = $random;
+							#0 b = $random;
+							#0 c = $random;
+							#0 Sum_corr = a + b + c;
+							#0 Sum = out;
+							if (Sum == Sum_corr)
+								Sum_correct = 1;
+							else
+								Sum_correct = 0;
+					end
+					@(negedge clk)
+					begin
+					$fdisplay(desc3, "%h %h %h || %h | %h | %b", a, b, c, Sum, Sum_correct, (Sum == Sum_corr));
+					end
+			end // @(negedge clk)
+		end
+
+
+
+/*
+
+  
+   
+  // instantiate device under test
+   rca dut (a, b, c, sum, cout);
+
+   initial 
+     begin	
+		clk = 1'b1;
+		forever #10 clk = ~clk;
+     end
 
    initial
      begin
     
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
+	#0   a = 4'b0000;	
+	#0   b = 4'b0000;
 	#0   c = 1'b0;
 
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
+	#20  a = 4'b0000;
+	#0   b = 4'b0000;
 	#0   c = 1'b1;
 
-	#20  a = 1'b1;
-	#0   b = 1'b0;
+	#20  a = 4'b0000;
+	#0   b = 4'b0001;
+	#0   c = 1'b0;
+
+	#20  a = 4'b0000;
+	#0   b = 4'b0001;
 	#0   c = 1'b1;
 
-	#20  a = 1'b0;
-	#0   b = 1'b1;
+	#0   a = 4'b0001;	
+	#0   b = 4'b0000;
+	#0   c = 1'b0;
+
+	#20  a = 4'b1;
+	#0   b = 4'b0;
 	#0   c = 1'b1;
 
 	#20  a = 1'b1;
 	#0   b = 1'b1;
-	#0   c = 1'b1;		
+	#0   c = 1'b0;
 
+	#20  a = 1'b1;
+	#0   b = 1'b1;
+	#0   c = 1'b1;
 
-	
+	#20  a = $random;
+	#0   b = $random;
+	#0   c = $random;
+
+	#20  a = $random;
+	#0   b = $random;
+	#0   c = $random;
+
+	#20  a = $random;
+	#0   b = $random;
+	#0   c = $random;	
      end
-
+*/
    
 endmodule
